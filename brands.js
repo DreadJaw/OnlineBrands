b866a56064cb578f777f6187d35e06a0818fd73e
DreadJaw <thedreadjaw@gmail.com>
2019-12-09 19:34:57 -0500

    Updated brands

--- brands.js
+++ brands.js
@@ -3,18 +3,28 @@ const brandList = {
         name: 'Facebook',
         description: 'Create an account or log into Facebook. Connect with friends, family and other people you know. Share photos and videos, send messages and get updates.',
         color: '#3b5998',
-        icon: 'facebook-f'
+        icon: 'facebook-f',
+        link: 'https://www.facebook.com'
     },
     twitter: {
         name: 'Twitter',
         description: 'From breaking news and entertainment to sports and politics, get the full story with all the live commentary.',
         color: '#00aced',
-        icon: 'twitter'
+        icon: 'twitter',
+        link: 'https://twitter.com'
     },
     github: {
         name: 'Github',
         description: 'GitHub brings together the world\'s largest community of developers to discover, share, and build better software.',
         color: '#333333',
-        icon: 'github'
+        icon: 'github',
+        link: ''
+    },
+    apple: {
+        name: 'Apple',
+        description: 'Discover Apple and shop everything iPhone, iPad, Apple Watch, Mac and Apple TV. Explore accessories, entertainment and expert device support.',
+        color: '#555555',
+        icon: 'apple',
+        link: 'https://www.apple.com'
     }
 }
