3e9f76ed01c335430f7fc8aea87ae3bd0ba5b7a2
DreadJaw <thedreadjaw@gmail.com>
2019-12-09 19:54:50 -0500

    Added several more brands

--- brands.js
+++ brands.js
@@ -26,5 +26,47 @@ const brandList = {
         color: '#555555',
         icon: 'apple',
         link: 'https://www.apple.com'
+    },
+    discord: {
+        name: 'Discord',
+        description: 'Get the best, most secure voice and text chat for playing games with your friends. It\'s quick and easy to set up, and 100% free!',
+        color: '#7289da',
+        icon: 'discord',
+        link: 'https://discordapp.com'
+    },
+    google: {
+        name: 'Google',
+        description: 'Search the world\'s information, including webpages, images, videos and more.',
+        color: '#db4437',
+        icon: 'google',
+        link: 'https://google.com/'
+    },
+    adobe: {
+        name: 'Adobe',
+        description: 'When imagination & innovation come together, anything’s possible.',
+        color: '#ff0000',
+        icon: 'adobe',
+        link: 'https://www.adobe.com'
+    },
+    amazon: {
+        name: 'Amazon',
+        description: 'Checkout Bestsellers from Top-rated Brands.',
+        color: '#ff9900',
+        icon: 'amazon',
+        link: 'https://www.amazon.com'
+    },
+    deviantart: {
+        name: 'DeviantArt',
+        description: 'DeviantArt is the world\'s largest online social community for artists and art enthusiasts, allowing people to connect through the creation and sharing of art.',
+        color: '#00e59b',
+        icon: 'deviantart',
+        link: 'https://www.deviantart.com'
+    },
+    patreon: {
+        name: 'Patreon',
+        description: 'Patreon is a membership platform that makes it easy for artists and creators to get paid.',
+        color: '#f96854',
+        icon: 'patreon',
+        link: 'https://www.patreon.com'
     }
 }
