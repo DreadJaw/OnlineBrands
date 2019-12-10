e31e762a60887d9cad83fe67fab53a51f9c6eaef
DreadJaw <thedreadjaw@gmail.com>
2019-12-09 19:17:52 -0500

    Started brands file

---
+++ brands.js
@@ -0,0 +1,20 @@
+const brandList = {
+    facebook: {
+        name: 'Facebook',
+        description: 'Create an account or log into Facebook. Connect with friends, family and other people you know. Share photos and videos, send messages and get updates.',
+        color: '#3b5998',
+        icon: 'facebook-f'
+    },
+    twitter: {
+        name: 'Twitter',
+        description: 'From breaking news and entertainment to sports and politics, get the full story with all the live commentary.',
+        color: '#00aced',
+        icon: 'twitter'
+    },
+    github: {
+        name: 'Github',
+        description: 'GitHub brings together the world\'s largest community of developers to discover, share, and build better software.',
+        color: '#333333',
+        icon: 'github'
+    }
+}
